b91b0fef9644564288de40662c4e983c9f433836
Nargis Fatima <nargis@modfxmedia.com>
2026-09-16 19:28:37 +0500

    MS calrity code

--- app/layout.tsx
+++ app/layout.tsx
@@ -95,6 +95,15 @@ export default function RootLayout({
             gtag('config', 'G-GX8PHZR3EG');
           `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "yj6pifnu2p");
+          `}
+        </Script>
         <BookingProvider>
           <ScrollToTop />
           <Header />
